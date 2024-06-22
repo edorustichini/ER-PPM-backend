4d9a83e337dab1c1077c42b18688bee62e8ac05f
edorustichini <edorusti@gmail.com>
2024-06-22 11:19:52 +0200

    errore immagini

--- elaborato_backend/settings.py
+++ elaborato_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3lw24pxm5!=zlf670drm)p=#-s=$jix-p0z$6xz_!mz3$gu7&l')
 
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
